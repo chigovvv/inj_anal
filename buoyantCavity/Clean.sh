1d3708e74060a62bc8539b69c95567cd5ca6c64e
chigovvv <kozhettop22@gmail.com>
2025-04-15 15:17:54 +0300

    init

---
+++ buoyantCavity/Clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/abdurakhmanov/OpenFOAM/abdurakhmanov-11/run/buoyantCavity/ 
+
+rm -rf 1* 
+rm -rf 2* 
+rm -rf 3* 
+rm -rf 4* 
+rm -rf 5* 
+rm -rf 6* 
+rm -rf 7* 
+rm -rf 8* 
+rm -rf 9* 
+rm -rf log 
+rm -rf constant/polyMesh 
+
+echo clean
